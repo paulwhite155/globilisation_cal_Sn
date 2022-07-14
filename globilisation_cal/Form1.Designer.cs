e01101ef985cf46624b170b5fbb71377242c5595
paulwhite155 <105815022+paulwhite155@users.noreply.github.com>
2022-07-14 15:47:24 +1200

    Add project files.

---
+++ globilisation_cal/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace globilisation_cal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.daymonth_label1 = new System.Windows.Forms.Label();
+            this.dayweek_label1 = new System.Windows.Forms.Label();
+            this.month_label2 = new System.Windows.Forms.Label();
+            this.year_label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "year";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 28);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "month";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(308, 120);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "day";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(459, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(459, 133);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(459, 75);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(445, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 33);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "get info";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(644, 133);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(644, 75);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(169, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.year_label1);
+            this.panel1.Controls.Add(this.daymonth_label1);
+            this.panel1.Controls.Add(this.dayweek_label1);
+            this.panel1.Controls.Add(this.month_label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(141, 165);
+            this.panel1.TabIndex = 9;
+            // 
+            // daymonth_label1
+            // 
+            this.daymonth_label1.AutoSize = true;
+            this.daymonth_label1.BackColor = System.Drawing.Color.Transparent;
+            this.daymonth_label1.Font = new System.Drawing.Font("Candara Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.daymonth_label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.daymonth_label1.Location = new System.Drawing.Point(24, 83);
+            this.daymonth_label1.Name = "daymonth_label1";
+            this.daymonth_label1.Size = new System.Drawing.Size(36, 29);
+            this.daymonth_label1.TabIndex = 3;
+            this.daymonth_label1.Text = "16";
+            // 
+            // dayweek_label1
+            // 
+            this.dayweek_label1.AutoSize = true;
+            this.dayweek_label1.BackColor = System.Drawing.Color.Transparent;
+            this.dayweek_label1.Font = new System.Drawing.Font("Candara Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayweek_label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dayweek_label1.Location = new System.Drawing.Point(24, 115);
+            this.dayweek_label1.Name = "dayweek_label1";
+            this.dayweek_label1.Size = new System.Drawing.Size(93, 29);
+            this.dayweek_label1.TabIndex = 2;
+            this.dayweek_label1.Text = "tuesday";
+            // 
+            // month_label2
+            // 
+            this.month_label2.AutoSize = true;
+            this.month_label2.BackColor = System.Drawing.Color.Transparent;
+            this.month_label2.Font = new System.Drawing.Font("Candara Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.month_label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.month_label2.Location = new System.Drawing.Point(24, 45);
+            this.month_label2.Name = "month_label2";
+            this.month_label2.Size = new System.Drawing.Size(61, 29);
+            this.month_label2.TabIndex = 1;
+            this.month_label2.Text = "june ";
+            // 
+            // year_label1
+            // 
+            this.year_label1.AutoSize = true;
+            this.year_label1.BackColor = System.Drawing.Color.Transparent;
+            this.year_label1.Font = new System.Drawing.Font("Candara Light", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.year_label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.year_label1.Location = new System.Drawing.Point(24, 14);
+            this.year_label1.Name = "year_label1";
+            this.year_label1.Size = new System.Drawing.Size(63, 29);
+            this.year_label1.TabIndex = 4;
+            this.year_label1.Text = "2020";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label month_label2;
+        private System.Windows.Forms.Label daymonth_label1;
+        private System.Windows.Forms.Label dayweek_label1;
+        private System.Windows.Forms.Label year_label1;
+    }
+}
+
---
+++ globilisation_cal/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace globilisation_cal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //System.Globalization.GregorianCalendar g = new System.Globalization.GregorianCalendar();  you could use this way
+            System.Globalization.GregorianCalendar g;
+            g = new System.Globalization.GregorianCalendar();
+            this.textBox1.Text = g.GetYear(DateTime.Now).ToString();
+            this.textBox2.Text = g.GetMonth(DateTime.Now).ToString();
+            this.textBox3.Text = g.GetDayOfMonth(DateTime.Now).ToString();
+            this.textBox4.Text = g.GetDayOfWeek(DateTime.Now).ToString();
+
+            Int32 m;
+            Int32.TryParse(this.textBox2.Text, out m);
+
+            this.textBox5.Text = month_num_to_name(m);
+
+
+        }
+            
+
+        
+
+        //------------------------------
+         public string month_num_to_name(Int32 m )
+        {
+
+            string a;
+            switch (m)
+            {
+                case 1:
+                    a = "January";
+                    break;
+                case 2:
+                    a = "Febrary";
+                    break;
+                case 3:
+                    a = "March";
+                    break;
+                case 4:
+                    a = "april";
+                    break;
+                case 5:
+                    a = "may ";
+                    break;
+                case 6:
+                    a = "june";
+                    break;
+                case 7:
+                    a = "July";
+                    break;
+                case 8:
+                    a = "August";
+                    break;
+                case 9:
+                    a = "September";
+                    break;
+                case 10:
+                    a = "october ";
+                    break;
+                case 11:
+                    a = "novemeber";
+                    break;
+                case 12:
+                    a = "decemeber";
+                    break;
+                default:
+                    a = "unknown";
+                    break;
+             }
+            return a;
+        
+         }
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Globalization.GregorianCalendar g;
+            g = new System.Globalization.GregorianCalendar();
+            this.year_label1.Text = g.GetYear(DateTime.Now).ToString();
+           
+            //this.textBox2.Text= g.GetMonth(DateTime.Now).ToString();
+            this.daymonth_label1.Text = g.GetDayOfMonth(DateTime.Now).ToString();
+            this.dayweek_label1.Text = g.GetDayOfWeek(DateTime.Now).ToString();
+
+            Int32 m;
+            Int32.TryParse(g.GetMonth(DateTime.Now).ToString(), out m);
+
+            this.month_label2.Text = month_num_to_name(m); 
+        }
+    }
+    
+}
